98f9cfa60b715fe4ff829336f9fd97130cdaac75
Minh Nguyen <xiuquay55@gmail.com>
2021-06-24 15:36:11 +0700

    building error manager

--- utils/errors/index.js
+++ utils/errors/index.js
@@ -1,4 +1,5 @@
 'use strict';
 
 exports.BaseError = require('./base-error');
+exports.NotFoundConfigError = require('./not-found-config-error');
 exports.NotFoundModuleError = require('./not-found-module-error');
---
+++ utils/errors/not-found-config-error.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const BaseError = require('./base-error');
+
+class NotFoundConfigError extends BaseError {
+  constructor(message) {
+    super(message);
+
+    this.name = 'ConfigNotFound'
+    this.message = message;
+  }
+};
+
+module.exports = NotFoundConfigError;
