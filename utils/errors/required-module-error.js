d6805eeb10d39cd7f7ebe3b1941f9483eac7b377
Minh Nguyen <xiuquay55@gmail.com>
2021-06-28 17:28:38 +0700

    modify code

--- utils/errors/required-module-error.js
+++ utils/errors/required-module-error.js
@@ -4,8 +4,6 @@ const BaseError = require('./base-error');
 
 class RequiredModuleError extends BaseError {
   constructor(error, module) {
-    console.log("🚀 ~ file: required-module-error.js ~ line 7 ~ RequiredModuleError ~ constructor ~ module", module)
-    console.log("🚀 ~ file: required-module-error.js ~ line 7 ~ RequiredModuleError ~ constructor ~ error", error)
     super(error, module);
     this.name = 'RequiredModuleError';
     this.message = `Please install ${module} in your package`;
